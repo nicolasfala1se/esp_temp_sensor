cfdba74ae71a6a01331e082910a22ee010490a0b
unknown <nicfalaise@gmail.com>
2020-02-14 15:50:39 -0600

    add spareroom config

---
+++ user_config/spareroom/user_config.py
@@ -0,0 +1,20 @@
+
+
+default_user_config = {
+    "WIFI_SSID": "ATTCM9wD7i",
+    "WIFI_PASS": "vi%5x4x4ueta",
+    "MQTT_SERVER": "192.168.1.86",
+    "NODE_NAME": "spareroom",
+    "WAKEUP_PERIOD": "30",
+    "REFRESH_COUNTER": "2",
+    "DEEPSLEEP_MODE": "DISABLE",
+    "DEBUG_MODE" : "1"
+}
+
+default_hardware_config = {
+    "DHT_PIN": "18",    # scl for bme280
+    "BME_PIN": "23",    # sda for bme280
+    "LED_PIN": "0",
+    "DHT_TYPE": "BME280",
+    "DEEPSLEEP_CAP": "DISABLE"
+}
